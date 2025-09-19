95b0f21f99923b5a315222139d7d0edcbdfd6651
Alejandro <alejandro_bautistame.@fet.edu.co>
2025-09-18 20:57:24 -0500

    Trabajho

---
+++ src/Figura.java
@@ -0,0 +1,39 @@
+interface Figura {
+    double calcularArea();
+    double calcularPerimetro();
+}
+class Cuadrado implements Figura {
+    private double lado;
+
+    public Cuadrado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 4 * lado;
+    }
+}
+class Circulo implements Figura {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+}
+
